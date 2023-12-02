827ab0ee43310fcf0284f5e9c5ec17be9444831a
Franco Osman <francoosman12@gmail.com>
2023-12-02 11:39:50 -0300

    Cambios en publicidad

--- src/components/Publicidad.jsx
+++ src/components/Publicidad.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Publicidad = ({ onClose }) => {
     return (
-        <section className="bg-no-repeat  bg-contain lg:bg-cover bg-center min-h-screen" style={{ backgroundImage: "url(https://i.ibb.co/QFJ28P7/Mira-nuestras-ofertas.gif)" }} id='RRHH'>
+        <section className="bg-no-repeat  bg-contain lg:bg-cover bg-center min-h-screen" style={{ backgroundImage: "url(https://i.ibb.co/KXbmB1z/Mira-nuestras-ofertas1.gif)" }} id='RRHH'>
 
             <button className='bg-red-500 rounded-lg p-2 text-white m-5' onClick={onClose}>Cerrar</button>
         </section>
